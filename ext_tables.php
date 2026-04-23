a3d2bd76ad31f444b5c925f46d6381081b976505
Philipp Kitzberger <coding@kitze.net>
2026-04-23 10:42:13 +0200

    Remove csh (that's gone with TYPO3 13)

--- ext_tables.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
-
-ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tt_content.pi_flexform.flipbook_pi1.list',
-    'EXT:flipbook/Resources/Private/Language/locallang_csh_flexform_pi1.xlf'
-);
